e3e594e27f9b39ff40db6fe2f3e7c4adc71df268
Ferdia McKeogh <ferdia@mckeogh.tech>
2019-02-06 20:01:52 +0100

    fix: use register writes not burst

--- src/lib.rs
+++ src/lib.rs
@@ -260,59 +260,55 @@ where
     pub fn set_500k_preset(&mut self) -> Result<(), Error<E>> {
         self.0.cs.set_low();
 
-        self.0.spi.write(&[0x40 | 0x40,
-            0x07, // IOCFG2        GDO2 Output Pin Configuration
-            0x2E, // IOCFG1        GDO1 Output Pin Configuration
-            0x80, // IOCFG0        GDO0 Output Pin Configuration
-            0x07, // FIFOTHR       RX FIFO and TX FIFO Thresholds
-            0x57, // SYNC1         Sync Word, High Byte
-            0x43, // SYNC0         Sync Word, Low Byte
-            0x3E, // PKTLEN        Packet Length
-            0x0E, // PKTCTRL1      Packet Automation Control
-            0x45, // PKTCTRL0      Packet Automation Control
-            0xFF, // ADDR          Device Address
-            0x00, // CHANNR        Channel Number
-            0x0C, // FSCTRL1       Frequency Synthesizer Control
-            0x00, // FSCTRL0       Frequency Synthesizer Control
-            0x21, // FREQ2         Frequency Control Word, High Byte
-            0x65, // FREQ1         Frequency Control Word, Middle Byte
-            0x6A, // FREQ0         Frequency Control Word, Low Byte
-            0x0E, // MDMCFG4       Modem Configuration
-            0x3B, // MDMCFG3       Modem Configuration
-            0x73, // MDMCFG2       Modem Configuration
-            0xA0, // MDMCFG1       Modem Configuration
-            0xF8, // MDMCFG0       Modem Configuration
-            0x00, // DEVIATN       Modem Deviation Setting
-            0x07, // MCSM2         Main Radio Control State Machine Configuration
-            0x0C, // MCSM1         Main Radio Control State Machine Configuration
-            0x18, // MCSM0         Main Radio Control State Machine Configuration
-            0x1D, // FOCCFG        Frequency Offset Compensation Configuration
-            0x1C, // BSCFG         Bit Synchronization Configuration
-            0xC7, // AGCCTRL2      AGC Control
-            0x40, // AGCCTRL1      AGC Control
-            0xB2, // AGCCTRL0      AGC Control
-            0x02, // WOREVT1       High Byte Event0 Timeout
-            0x26, // WOREVT0       Low Byte Event0 Timeout
-            0x09, // WORCTRL       Wake On Radio Control
-            0xB6, // FREND1        Front End RX Configuration
-            0x17, // FREND0        Front End TX Configuration
-            0xEA, // FSCAL3        Frequency Synthesizer Calibration
-            0x0A, // FSCAL2        Frequency Synthesizer Calibration
-            0x00, // FSCAL1        Frequency Synthesizer Calibration
-            0x19, // FSCAL0        Frequency Synthesizer Calibration
-            0x41, // RCCTRL1       RC Oscillator Configuration
-            0x00, // RCCTRL0       RC Oscillator Configuration
-            0x59, // FSTEST        Frequency Synthesizer Calibration Control,
-            0x7F, // PTEST         Production Test
-            0x3F, // AGCTEST       AGC Test
-            0x81, // TEST2         Various Test Settings
-            0x3F, // TEST1         Various Test Settings
-            0x0B  // TEST0         Various Test Settings
-        ])?;
+        self.0.write_register(Config::IOCFG2, 0x07)?; // IOCFG2        GDO2 Output Pin Configuration
+        self.0.write_register(Config::IOCFG1, 0x2E)?; // IOCFG1        GDO1 Output Pin Configuration
+        self.0.write_register(Config::IOCFG0, 0x80)?; // IOCFG0        GDO0 Output Pin Configuration
+        self.0.write_register(Config::FIFOTHR, 0x07)?; // FIFOTHR       RX FIFO and TX FIFO Thresholds
+        self.0.write_register(Config::SYNC1, 0x57)?; // SYNC1         Sync Word High Byte
+        self.0.write_register(Config::SYNC0, 0x43)?; // SYNC0         Sync Word Low Byte
+        self.0.write_register(Config::PKTLEN, 0xFD)?; // PKTLEN        Packet Length
+        self.0.write_register(Config::PKTCTRL1, 0x0E)?; // PKTCTRL1      Packet Automation Control
+        self.0.write_register(Config::PKTCTRL0, 0x45)?; // PKTCTRL0      Packet Automation Control
+        self.0.write_register(Config::ADDR, 0xFF)?; // ADDR          Device Address
+        self.0.write_register(Config::CHANNR, 0x00)?; // CHANNR        Channel Number
+        self.0.write_register(Config::FSCTRL1, 0x0C)?; // FSCTRL1       Frequency Synthesizer Control
+        self.0.write_register(Config::FSCTRL0, 0x00)?; // FSCTRL0       Frequency Synthesizer Control
+        self.0.write_register(Config::MDMCFG4, 0x0E)?; // MDMCFG4       Modem Configuration
+        self.0.write_register(Config::MDMCFG3, 0x3B)?; // MDMCFG3       Modem Configuration
+        self.0.write_register(Config::MDMCFG2, 0x73)?; // MDMCFG2       Modem Configuration
+        self.0.write_register(Config::MDMCFG1, 0xA0)?; // MDMCFG1       Modem Configuration
+        self.0.write_register(Config::MDMCFG0, 0xF8)?; // MDMCFG0       Modem Configuration
+        self.0.write_register(Config::DEVIATN, 0x00)?; // DEVIATN       Modem Deviation Setting
+        self.0.write_register(Config::MCSM2, 0x07)?; // MCSM2         Main Radio Control State Machine Configuration
+        self.0.write_register(Config::MCSM1, 0x0C)?; // MCSM1         Main Radio Control State Machine Configuration
+        self.0.write_register(Config::MCSM0, 0x18)?; // MCSM0         Main Radio Control State Machine Configuration
+        self.0.write_register(Config::FOCCFG, 0x1D)?; // FOCCFG        Frequency Offset Compensation Configuration
+        self.0.write_register(Config::BSCFG, 0x1C)?; // BSCFG         Bit Synchronization Configuration
+        self.0.write_register(Config::AGCCTRL2, 0xC7)?; // AGCCTRL2      AGC Control
+        self.0.write_register(Config::AGCCTRL1, 0x40)?; // AGCCTRL1      AGC Control
+        self.0.write_register(Config::AGCCTRL0, 0xB2)?; // AGCCTRL0      AGC Control
+        self.0.write_register(Config::WOREVT1, 0x02)?; // WOREVT1       High Byte Event0 Timeout
+        self.0.write_register(Config::WOREVT0, 0x26)?; // WOREVT0       Low Byte Event0 Timeout
+        self.0.write_register(Config::WORCTRL, 0x09)?; // WORCTRL       Wake On Radio Control
+        self.0.write_register(Config::FREND1, 0xB6)?; // FREND1        Front End RX Configuration
+        self.0.write_register(Config::FREND0, 0x17)?; // FREND0        Front End TX Configuration
+        self.0.write_register(Config::FSCAL3, 0xEA)?; // FSCAL3        Frequency Synthesizer Calibration
+        self.0.write_register(Config::FSCAL2, 0x0A)?; // FSCAL2        Frequency Synthesizer Calibration
+        self.0.write_register(Config::FSCAL1, 0x00)?; // FSCAL1        Frequency Synthesizer Calibration
+        self.0.write_register(Config::FSCAL0, 0x19)?; // FSCAL0        Frequency Synthesizer Calibration
+        self.0.write_register(Config::RCCTRL1, 0x41)?; // RCCTRL1       RC Oscillator Configuration
+        self.0.write_register(Config::RCCTRL0, 0x00)?; // RCCTRL0       RC Oscillator Configuration
+        self.0.write_register(Config::FSTEST, 0x59)?; // FSTEST        Frequency Synthesizer Calibration Control
+        self.0.write_register(Config::PTEST, 0x7F)?; // PTEST         Production Test
+        self.0.write_register(Config::AGCTEST, 0x3F)?; // AGCTEST       AGC Test
+        self.0.write_register(Config::TEST2, 0x81)?; // TEST2         Various Test Settings
+        self.0.write_register(Config::TEST1, 0x3F)?; // TEST1         Various Test Settings
+        self.0.write_register(Config::TEST0, 0x0B)?; // TEST0         Various Test Settings
 
-        self.0.spi.write(&[0x7E | 0x40,
-        0x17, 0x1D, 0x26, 0x69, 0x51, 0x86, 0xCC, 0xC3])?;
+        // PATABLE
+        self.0.spi.write(&[0x7E | 0x40, 0x17, 0x1D, 0x26, 0x69, 0x51, 0x86, 0xCC, 0xC3])?;
 
+        // 315MHz
         self.0.write_register(Config::FREQ2, 0x0C)?;
         self.0.write_register(Config::FREQ1, 0x1D)?;
         self.0.write_register(Config::FREQ0, 0x89)?;
@@ -325,23 +321,20 @@ where
         Ok(())
     }
 
-    /// ?
+    /// Transmit a raw slice, the length 
     pub fn transmit(&mut self, buf: &mut [u8]) -> Result<(), Error<E>> {
         let len = buf.len();
-        if len > 61 {
-            return Err(Error::PayloadOversize);
-        }
 
         self.0.buf[0] = 0x7F | 0x40;
         self.0.buf[1] = len as u8 + 1;
         self.0.buf[2] = 0x02;
         for i in 0..len {
-            self.0.buf[3+i] = buf[i];
+            self.0.buf[3 + i] = buf[i];
         }
 
         self.0.cs.set_low();
 
-        self.0.spi.write(&self.0.buf[..len+3])?;
+        self.0.spi.write(&self.0.buf[..len + 3])?;
 
         self.0.spi.write(&[0x35])?;
         self.await_machine_state(MachineState::IDLE)?;
